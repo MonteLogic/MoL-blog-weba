c4cedbf80f4c9954e7b4783943a874a7b90e5427
MonteLogic <dchavours@gmail.com>
2025-03-28 13:37:10 -0600

    ts-ignore

--- ui/markdown-client-render.tsx
+++ ui/markdown-client-render.tsx
@@ -16,11 +16,13 @@ export default function MarkdownContent({ content }: MarkdownContentProps) {
       className="markdown-content"
       components={{
         // You can customize how different elements are rendered
+        // @ts-ignore
         code: ({ node, inline, className, children, ...props }) => {
           const match = /language-(\w+)/.exec(className || '');
           return !inline && match ? (
             <div className="code-block-wrapper">
               <div className="code-language">{match[1]}</div>
+              {/* @ts-ignore */}
               <pre className={className} {...props}>
                 <code className={className} {...props}>
                   {children}
