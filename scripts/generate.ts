3345190ac35b2e8b069e000683e44471cccfe791
MonteLogic <dchavours@gmail.com>
2025-05-05 15:34:10 -0500

    scripts file

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -2,8 +2,10 @@
 import { generateMarkdownPaths } from '#/utils/paths/markdownPaths';
 import * as path from 'path';
 
+
+// Don't like the hard-coded string part of projectRoot
 // Define your markdown directory and output file path relative to the project root
-const projectRoot = path.join(__dirname, '../');
+const projectRoot = path.join(__dirname, '../MoL-blog-content');
 const markdownSourceDir = path.join(projectRoot, 'path/to/your/submodule'); // **Adjust this path**
 const outputJsonFile = path.join(projectRoot, 'data/markdown-paths.json'); // **Adjust this path
 const baseDirectoryForPaths = projectRoot; // Optional: make paths in JSON relative to project root
